600c724f26a20680bfdcfdc24531a0a736b45876
Eduardo Imery <ejwimery@gmail.com>
2019-03-01 18:30:52 -0600

    edicion de vista trainer

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -3,12 +3,12 @@
 @section('title','Trainers')
     
 @section('content')
-    <br>
+    
     <div class="row">
         @foreach ($trainers as $trainer)
             <div class="col-sm">
-                    <div class="card" style="width: 18rem;">
-                    <img src="images/{{$trainer->avatar}}" class="card-img-top" alt="...">
+                    <div class="card text-center" style="width: 18rem; margin-top: 70px">
+                    <img style="height: 100px; width:100px; margin:20px" src="images/{{$trainer->avatar}}" class="card-img-top rounded-circle mx-auto d-block" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">{{$trainer->name}}</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
