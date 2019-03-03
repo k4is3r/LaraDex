beefb39b8f644450b3ad9d9c671a18e366859b7c
Eduardo Imery <ejwimery@gmail.com>
2019-03-02 18:30:49 -0600

    usando sub views para reciclar codigo

--- resources/views/trainers/edit.blade.php
+++ resources/views/trainers/edit.blade.php
@@ -4,19 +4,8 @@
     
 @section('content')
 {!! Form::model($trainer,['route' => ['trainers.update', $trainer],'method'=>'PUT', 'files'=>true]) !!}
-        <div class="form-group">
-            {!! Form::label('name', 'Nombre') !!}
-            {!! Form::text('name', null, ['class'=> 'form-control'])!!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('slug', 'Slug') !!}
-            {!! Form::text('slug', null, ['class'=> 'form-control'])!!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('avatar', 'Avatar') !!}
-            {!! Form::file('avatar')!!}
-            <img style="height: 200px; width:200px; margin:20px" src="/images/{{$trainer->avatar}}" class="card-img-top rounded-circle mx-auto d-block" alt="...">
-        </div>
+        @include('trainers.form')
+        <img style="height: 200px; width:200px; margin:20px" src="/images/{{$trainer->avatar}}" class="card-img-top rounded-circle mx-auto d-block" alt="...">
         {!! Form::submit('Actualizar',['class'=>'btn btn-primary'])!!}
     {!! Form::close() !!}
     
